7bd6ce3e18be6ed8dda3aebc4fd93bc7bb268c42
balarabe@protonmail.com <balarabe@protonmail.com>
2018-02-26 14:24:42 +0100

    calendar.go: make AddMonth() return error; add TOC and comments; rename/reorder things

--- calendar.go
+++ calendar.go
@@ -1,9 +1,25 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2018-02-23 18:08:54 42669E                               [zr/calendar.go]
+// :v: 2018-02-26 14:12:39 F37C8B                               [zr/calendar.go]
 // -----------------------------------------------------------------------------
 
 package zr
+
+// # Types
+//   Calendar struct
+//   calendarDay struct
+//   calendarMonth struct
+//   calendarWeekdaysEN = []string
+//
+// # Methods
+//  (ob *Calendar) AddMonth(year int, month time.Month) error
+//  (ob *Calendar) HasMonth(year int, month time.Month) bool
+//  (ob *Calendar) Set(date, value interface{})
+//  (ob *Calendar) String() string
+//
+// # Internal Methods/Functions
+//  (_ *Calendar) firstWeekday(year int, month time.Month) time.Weekday
+//  (ob *Calendar) getMonth(year int, month time.Month) *calendarMonth
 
 import "bytes" // standard
 import "fmt"   // standard
@@ -12,8 +28,30 @@ import "time"  // standard
 // -----------------------------------------------------------------------------
 // # Types
 
-// cldrWeekdaysEN __
-var cldrWeekdaysEN = []string{
+// Calendar provides logic for generating
+// calendar grids from dates and values.
+type Calendar struct {
+	months []calendarMonth
+} //                                                                    Calendar
+
+// calendarDay holds the calendar entry for a single day
+// 'day' is day of the month (the date)
+// 'val' is the value shown on the specified date
+type calendarDay struct {
+	day int
+	val interface{}
+} //                                                                 calendarDay
+
+// calendarMonth holds the data a single month,
+// with days arranged in a 6 x 7 grid.
+type calendarMonth struct {
+	year  int
+	month time.Month
+	cells [6][7]calendarDay
+} //                                                               calendarMonth
+
+// calendarWeekdaysEN defines English weekday names
+var calendarWeekdaysEN = []string{
 	"Monday",
 	"Tuesday",
 	"Wednesday",
@@ -21,45 +59,23 @@ var cldrWeekdaysEN = []string{
 	"Friday",
 	"Saturday",
 	"Sunday",
-} //                                                              cldrWeekdaysEN
-
-// Calendar __
-// a 6 x 7 grid
-type Calendar struct {
-	months []cldrMonth
-} //                                                                    Calendar
-
-// cldrMonth __
-type cldrMonth struct {
-	year  int
-	month time.Month
-	cells [6][7]cldrDay
-} //                                                                   cldrMonth
-
-// cldrDay __
-type cldrDay struct {
-	day int
-	val interface{}
-} //                                                                     cldrDay
+} //                                                          calendarWeekdaysEN
 
 // -----------------------------------------------------------------------------
-// # Factory
+// # Methods
 
-// NewCalendar __
-func NewCalendar() Calendar {
-	return Calendar{}
-} //                                                                 NewCalendar
-
-// AddMonth __
-func (ob *Calendar) AddMonth(year int, month time.Month) {
-	//
+// AddMonth adds a month to the calendar, without setting any values.
+// The year must range from 1 to 9999.
+func (ob *Calendar) AddMonth(year int, month time.Month) error {
+	if year < 1 || year > 9999 {
+		return Error(EInvalidArg, "^year", ":", year)
+	}
 	// check if month was added already
 	if ob.HasMonth(year, month) {
-		Error("Month", month, year, "already added")
-		return
+		return Error("Month", month, year, "already added")
 	}
-	var mth = cldrMonth{year: year, month: month}
-	var weekday1 = cldrFirstWeekday(year, month)
+	var mth = calendarMonth{year: year, month: month}
+	var weekday1 = ob.firstWeekday(year, month)
 	var last = DaysInMonth(year, month)
 	var day = 1
 loop:
@@ -76,6 +92,7 @@ loop:
 		}
 	}
 	ob.months = append(ob.months, mth)
+	return nil
 } //                                                                    AddMonth
 
 // HasMonth returns true if the month specified by
@@ -89,7 +106,8 @@ func (ob *Calendar) HasMonth(year int, month time.Month) bool {
 	return false
 } //                                                                    HasMonth
 
-// Set __
+// Set assigns the specified value to the specified date.
+// It automatically converts 'date' to time.Time
 func (ob *Calendar) Set(date, value interface{}) {
 	var dt = DateOf(date)
 	var year, month = dt.Year(), dt.Month()
@@ -101,7 +119,9 @@ func (ob *Calendar) Set(date, value interface{}) {
 	// get a pointer to the calendar
 	var mth = ob.getMonth(year, month)
 	if mth == nil {
-		return // (a warning is logged by getMonth)
+		// ^ this condition should never occur
+		// (no warning here: a warning is already logged by getMonth)
+		return
 	}
 	// find the specified day and set its value
 	var day = dt.Day()
@@ -116,8 +136,40 @@ func (ob *Calendar) Set(date, value interface{}) {
 	}
 } //                                                                         Set
 
-// String __
+// String returns the calendar as a text string
+// and implements the fmt.Stringer interface.
+//
+// The output may contain multiple months, in which
+// case the months are arranged in ascending order.
+//
+// See the sample output in the body of the function.
+//
 func (ob *Calendar) String() string {
+	// Sample output:
+	//
+	// 2018 FEBRUARY
+	// *--------------------------------------------------------------*
+	// |  Mon   |  Tue   |  Wed   |  Thu   |  Fri   |  Sat   |  Sun   |
+	// |--------|--------|--------|--------|--------|--------|--------|
+	// |        |        |        | 1      | 2      | 3      | 4      |
+	// |        |        |        |   8.44 |   7.55 |   6.66 |   5.77 |
+	// |--------|--------|--------|--------|--------|--------|--------|
+	// | 5      | 6      | 7      | 8      | 9      | 10     | 11     |
+	// |   4.88 |   3.99 |   2.15 |   1.54 |      0 |      1 |      2 |
+	// |--------|--------|--------|--------|--------|--------|--------|
+	// | 12     | 13     | 14     | 15     | 16     | 17     | 18     |
+	// |      3 |      4 |      5 |      6 |      7 |      8 |      9 |
+	// |--------|--------|--------|--------|--------|--------|--------|
+	// | 19     | 20     | 21     | 22     | 23     | 24     | 25     |
+	// |     10 |        |        |        |        |        |        |
+	// |--------|--------|--------|--------|--------|--------|--------|
+	// | 26     | 27     | 28     |        |        |        |        |
+	// |        |        |        |        |        |        |        |
+	// |--------|--------|--------|--------|--------|--------|--------|
+	// |        |        |        |        |        |        |        |
+	// |        |        |        |        |        |        |        |
+	// *--------------------------------------------------------------*
+	// 95.98
 	const EDGE = "*"
 	const HDIV = "-"
 	const VDIV = "|"
@@ -171,7 +223,7 @@ func (ob *Calendar) String() string {
 		// weekday names
 		for i := 0; i < 7; i++ {
 			ws(VDIV)
-			ws(fmt.Sprintf(wdayFmt, cldrWeekdaysEN[i][:3]))
+			ws(fmt.Sprintf(wdayFmt, calendarWeekdaysEN[i][:3]))
 		}
 		ws(VDIV, LF)
 		var sum float64
@@ -217,11 +269,18 @@ func (ob *Calendar) String() string {
 } //                                                                      String
 
 // -----------------------------------------------------------------------------
-// # Helper Method
+// # Internal Methods/Functions
+
+// firstWeekday returns the day of
+// week on the first of the given month,
+func (_ *Calendar) firstWeekday(year int, month time.Month) time.Weekday {
+	var date = time.Date(year, month, 1, 0, 0, 0, 0, time.UTC)
+	return date.Weekday()
+} //                                                                firstWeekday
 
 // getMonth returns a pointer to the month specified by 'year' and
 // 'month', or nil if the month has not been added to this calendar
-func (ob *Calendar) getMonth(year int, month time.Month) *cldrMonth {
+func (ob *Calendar) getMonth(year int, month time.Month) *calendarMonth {
 	for i, m := range ob.months {
 		if m.year == year && m.month == month {
 			return &ob.months[i]
@@ -230,15 +289,5 @@ func (ob *Calendar) getMonth(year int, month time.Month) *cldrMonth {
 	Error(ENotFound, "month", month, year)
 	return nil
 } //                                                                    getMonth
-
-// -----------------------------------------------------------------------------
-// # Helper Function
-
-// cldrFirstWeekday returns the day of
-// week on the first of the given month,
-func cldrFirstWeekday(year int, month time.Month) time.Weekday {
-	var date = time.Date(year, month, 1, 0, 0, 0, 0, time.UTC)
-	return date.Weekday()
-} //                                                            cldrFirstWeekday
 
 // end
