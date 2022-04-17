06b881516cf04cb14452d22f5182c10b88c95f3f
Ali Bala <balarabe@protonmail.com>
2022-04-17 20:44:43 +0100

    refactor(ByteSizeString): minor change

--- bytes_func.go
+++ bytes_func.go
@@ -59,9 +59,9 @@ func ByteSizeString(sizeInBytes int64, useSI bool) string {
 	if !useSI && sizeInBytes == math.MinInt64 {
 		return "-7.9 EiB"
 	}
-	neg := sizeInBytes < 0
+	negative := sizeInBytes < 0
 	var ret string
-	if neg {
+	if negative {
 		sizeInBytes = -sizeInBytes
 		ret = "-"
 	}
